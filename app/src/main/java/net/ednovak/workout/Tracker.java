d8d53f4831c961c0038507f2a9c079f5af8d9a5e
Ed <ejnovak@email.wm.edu>
2016-05-02 15:24:29 -0400

    Created A and B days.

--- app/src/main/java/net/ednovak/workout/Tracker.java
+++ app/src/main/java/net/ednovak/workout/Tracker.java
@@ -1,5 +1,6 @@
 package net.ednovak.workout;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -18,11 +19,15 @@ public class Tracker extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tracker);
+        prefs = getPreferences(MODE_PRIVATE);
+        String day = prefs.getString("day", "a");
+        if(day.equals("a")){
+            setContentView(R.layout.activity_tracker_a);
+        } else if (day.equals("b")){
+            setContentView(R.layout.activity_tracker_b);
+        }
 
         mainView = (LinearLayout)findViewById(R.id.mainView);
-        prefs = getPreferences(MODE_PRIVATE);
-
         for(int i = 0; i < mainView.getChildCount(); i++) {
             try {
                 if (mainView.getChildAt(i).getTag().toString().equals("exercise")) {
@@ -33,6 +38,8 @@ public class Tracker extends AppCompatActivity {
                 continue;
             }
         }
+
+        getSupportActionBar().setTitle(getSupportActionBar().getTitle() + " " + day.toUpperCase());
     }
 
     @Override
@@ -75,6 +82,22 @@ public class Tracker extends AppCompatActivity {
                 startActivity(intent);
                 return true;
 
+            case R.id.action_switchDay:
+                SharedPreferences.Editor editor = prefs.edit();
+                String day = prefs.getString("day", "a");
+                if(day.equals("a")){
+                    day = "b";
+                } else if (day.equals("b")){
+                    day = "a";
+                }
+                editor.putString("day", day);
+                editor.commit();
+                finish();
+                intent = new Intent(this, Tracker.class);
+                startActivity(intent);
+                return true;
+
+
             default:
                 Log.d(TAG, "Somehow a menu item that should not exist was selected: " + id);
         }
@@ -85,8 +108,13 @@ public class Tracker extends AppCompatActivity {
     @Override
     public void onStop(){
         super.onStop();
+    }
 
+    @Override
+    public void onPause(){
+        super.onPause();
 
+        // Save weights
         SharedPreferences.Editor editor = prefs.edit();
         for(int i = 0; i < mainView.getChildCount(); i++){
             try{
